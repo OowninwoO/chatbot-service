1fc00adff5d2f6f6ebcb741852123552a29e8d30
niw0906 <niw0906@naver.com>
2026-02-21 21:06:20 +0900

    proj: .env 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 const app = require("./app");
 
-const port = 3000;
+const port = Number(process.env.PORT);
 app.listen(port, () => {
   console.log(`chatbot-service running on http://localhost:${port}`);
 });
